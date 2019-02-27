5c7ce3afd4d4c998271bf18b4ccd81a697c901ef
EugeneGapanovich <47712560+EugeneGapanovich@users.noreply.github.com>
2019-02-28 01:16:15 +0300

    Add files via upload

---
+++ 21.02/0-2.cpp
@@ -0,0 +1,59 @@
+﻿//	В массиве размера N, заполненного случ.числами от 0 до 10, определить максимальную длину последовательности равных элементов.
+
+#include <iostream>
+#include <clocale>
+#include <ctime>
+
+using namespace std;
+
+void initArrRandom(int* Arr, int n, int a, int b) {
+	for (int i = 0; i < n; ++i) {
+		*(Arr + i) = rand() % ((b - a + 1) + a);
+	}
+	return;
+}
+void printArr(int* Arr, int n) {
+	for (int i = 0; i < n; ++i) {
+		cout << *(Arr + i) << ",";
+	}
+	cout << "\n";
+	return;
+}
+void countMaxLength(int* Arr, int n) {
+	int cnt1{ 1 }, cnt2{ 1 };
+	for (int i = 1; i < n;++i) {
+		if (*(Arr + i) == *(Arr + (i - 1))) {
+			while (*(Arr + i) == *(Arr + (i - 1))) {
+				++cnt1;
+				++i;
+			}
+		}
+		if (cnt1 > cnt2) {
+			cnt2 = cnt1;
+		}
+		cnt1 = 1;
+	}
+	cout << "Максимальная длина=" << cnt2 << "\n";
+	return;
+}
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+	int n;
+	cout << "Введите размер массива: ";
+	cin >> n;
+	int* Arr = new (nothrow) int[n];
+	if (!Arr) {
+		cout << "error!" << "\n";
+		system("pause");
+		return 0;
+	}
+	initArrRandom(Arr, n, 0, 10);
+	printArr(Arr, n);
+	countMaxLength(Arr, n);
+	delete[]Arr;
+	Arr = nullptr;
+	system("pause");
+	return 0;
+}
---
+++ 21.02/0-4.cpp
@@ -0,0 +1,61 @@
+﻿//	Расположить в порядке возрастания элементы массива А(N), начиная с k-го элемента.
+
+#include <iostream>
+#include <clocale>
+#include <ctime>
+
+using namespace std;
+
+void initArrRandom(int* Arr, int n) {
+	for (int i = 0; i < n; ++i) {
+		*(Arr + i) = rand() % 100;
+	}
+	return;
+}
+void printArr(int* Arr, int n) {
+	for (int i = 0; i < n; ++i) {
+		cout << *(Arr + i) << ",";
+	}
+	cout << "\n";
+	return;
+}
+void sortStartWithValueK(int* Arr, int n,int k) {
+	int tmp;
+	k--;
+	for (int i = k; i < n - 1; i++) {
+		for (int j = k; j < n - i - 1 + k; j++) {
+			if (*(Arr+j) > *(Arr + j + 1)) {
+				tmp = *(Arr + j);
+				*(Arr + j) = *(Arr + j + 1);
+				*(Arr + j + 1) = tmp;
+			}
+		}
+	}
+	return;
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+	int n, k;
+	cout << "Введите размер массива: ";
+	cin >> n;
+	cout << "Введите с какого элемента сортировать по возрастанию: ";
+	cin >> k;
+	int* Arr = new (nothrow) int[n];
+	if (!Arr) {
+		cout << "error!" << "\n";
+		system("pause");
+		return 0;
+	}
+
+	initArrRandom(Arr, n);
+	cout << "Изначальный массив: ";
+	printArr(Arr,n);
+	cout << "Отсортированный массив: ";
+	sortStartWithValueK(Arr, n, k);
+	printArr(Arr, n);
+	delete[]Arr;
+	Arr = nullptr;
+	system("pause");
+	return 0;
+}
