1c46b8e947bc5369b627a960a0001d1ba388a544
EugeneGapanovich <47712560+EugeneGapanovich@users.noreply.github.com>
2019-03-07 19:02:15 +0300

    Add files via upload
    Updated file

---
+++ 1-2.cpp
@@ -0,0 +1,76 @@
+﻿//	Положительные элементы массива А(N) переставить в конец массива, сохраняя порядок следования. 
+//	Отрицательные элементы расположить в порядке убывания. Дополнительный массив не использовать.
+
+#include<iostream>
+#include<ctime>
+#include <iomanip>
+
+using namespace std;
+
+int* createMemory(int n) {
+	int* Arr = new(nothrow) int[n];
+	if (!Arr) {
+		cout << "Error!" << "\n";
+		system("pause");
+		return 0;
+	}
+	return Arr;
+}
+void freeMemory(int*& Arr) {
+	delete[]Arr;
+	Arr = nullptr;
+	return;
+}
+void initArrRandom(int* Arr, int n) {
+	for (int i = 0; i < n; ++i) {
+		*(Arr + i) = rand() % 40 - rand() % 30;
+	}
+	return;
+}
+void printArr(int* Arr, int n) {
+	for (int i = 0; i < n; ++i) {
+		cout <<setw(4)<< *(Arr + i);
+	}
+	cout << "\n";
+	return;
+}
+void sort(int* Arr, int n) {
+	int cnt1{ 0 };
+	for (int i = n - 1; i >= 0; --i) {
+		if (*(Arr + i) > 0) {
+			swap(*(Arr + i), *(Arr + n - 1 - cnt1));
+			++cnt1;
+		}
+	}
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j) {
+			if (*(Arr + j) < *(Arr + j + 1)) {
+				if (*(Arr + j + 1) <= 0) {
+					swap(*(Arr + j), *(Arr + j + 1));
+				}
+			}
+		}
+	return;
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+	int n;
+	cout << "Введите размер массива: ";
+	cin >> n;
+	if (n <= 0) {
+		cout << "Пожалуйста, введите положительное число для размера массива" << "\n";
+		system("pause");
+		exit(0);
+	}
+	int*Arr=createMemory(n);
+	initArrRandom(Arr, n);
+	cout << "Исходный массив: " << "\n";
+	printArr(Arr, n);
+	sort(Arr, n);
+	cout << "Сортированный массив: " << "\n";
+	printArr(Arr, n);
+	freeMemory(Arr);
+	system("pause");
+	return 0;
+}
