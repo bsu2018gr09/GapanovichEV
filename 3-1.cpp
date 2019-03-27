9b4584e35f314aa1cbbb5d43b546f5ecceccba54
EugeneGapanovich <47712560+EugeneGapanovich@users.noreply.github.com>
2019-03-27 23:02:56 +0300

    Update 3-1.cpp

--- 3-1.cpp
+++ 3-1.cpp
@@ -1,4 +1,4 @@
-﻿// В строке после каждого слова с нечётной длиной, начинающегося с заданного символа, вставить заданную подстроку, например (”**”.. 
+// В строке после каждого слова с нечётной длиной, начинающегося с заданного символа, вставить заданную подстроку, например (”**”.. 
 
 #define _CRT_SECURE_NO_WARNINGS 
 #include <iostream> 
@@ -9,9 +9,67 @@ using namespace std;
 
 const int MAX = 255;
 
+void init_string(char* &str);
+void init_subString(char* &str);
+void init_dvymerniy_array(char**& arr, int k);
+void enter_letter(char &letter);
 int findNumberOfWords(char* str, int cnt);
 void arrayFill(char* str, char** arr);
 void addSubStr(char** arr, char letter, char* subStr, int k);
+void free_memory(char* &str);
+void free_memory_dvymerniy_array(char**&arr, int k);
+
+void init_string(char* &str) {
+	char buf1[MAX];
+	cout << "Введите пожалуйста строку:\n";
+	cin.getline(buf1, MAX);
+	str = new (nothrow) char[strlen(buf1) + 1];
+	if (!str) {
+		cout << "No memory \n";
+		system("pause");
+		exit(1);
+	}
+	strcpy(str, buf1);
+	return;
+}
+void init_subString(char* &str) {
+	char buf2[MAX];
+	cout << "Введите пожалуйста подстроку:\n";
+	cin.getline(buf2, MAX);
+	str = new (nothrow) char[strlen(buf2) + 1];
+	if (!str) {
+		cout << "No memory \n";
+		system("pause");
+		exit(1);
+	}
+	strcpy(str, buf2);
+	return;
+}
+
+void init_dvymerniy_array(char**& arr, int k) {
+	arr = new (nothrow) char*[k];
+	if (!arr) {
+		cout << "No memory \n";
+		system("pause");
+		exit(1);
+	}
+	for (int i{ 0 }; i < k; ++i)
+	{
+		arr[i] = new (nothrow) char[MAX];
+		if (!arr[i]) {
+			cout << "No memory \n";
+			system("pause");
+			exit(1);
+		}
+	}
+	return;
+}
+
+void enter_letter(char &letter) {
+	cout << "Введите пожалуйста символ начала слова, по которому будет проходить отбор:\n";
+	cin >> letter;
+	return;
+}
 
 int findNumberOfWords(char* str, int cnt)
 {
@@ -63,58 +121,13 @@ void addSubStr(char** arr, char letter, char* subStr, int k)
 	return;
 }
 
-int main()
-{
-	setlocale(LC_ALL, "ru");
-	int cnt{ 0 }, k;
-	char buf1[MAX], buf2[MAX], letter;
-
-	cout << "Введите пожалуйста строку:\n";
-	cin.getline(buf1, MAX);
-	char* str = new (nothrow) char[strlen(buf1) + 1];
-	if (!str) {
-		cout << "No memory \n";
-		system("pause");
-		exit(1);
-	}
-	strcpy(str, buf1);
-
-	cout << "Введите пожалуйста подстроку:\n";
-	cin.getline(buf2, MAX);
-	char* subStr = new (nothrow) char[strlen(buf2) + 1];
-	if (!subStr) {
-		cout << "No memory \n";
-		system("pause");
-		exit(1);
-	}
-	strcpy(subStr, buf2);
-
-	cout << "Введите пожалуйста символ начала слова, по которому будет проходить отбор:\n";
-	cin >> letter;
-
-	k = findNumberOfWords(str, cnt);
-
-	char** arr = new (nothrow) char*[k];
-	if (!arr) {
-		cout << "No memory \n";
-		system("pause");
-		exit(1);
-	}
-	for (int i{ 0 }; i < k; ++i)
-	{
-		arr[i] = new (nothrow) char[MAX];
-		if (!arr[i]) {
-			cout << "No memory \n";
-			system("pause");
-			exit(1);
-		}
-	}
-
-	arrayFill(str, arr);
-	addSubStr(arr, letter, subStr, k);
-
+void free_memory(char* &str) {
 	delete[]str;
-	delete[]subStr;
+	str = nullptr;
+	return;
+}
+
+void free_memory_dvymerniy_array(char**&arr, int k) {
 	for (int i = 0; i < k; ++i)
 	{
 		delete[] arr[i];
@@ -122,8 +135,31 @@ int main()
 	}
 	delete[] arr;
 	arr = nullptr;
-	str = nullptr;
-	subStr = nullptr;
+	return;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int cnt{ 0 }, k;
+	char letter;
+	char* str = nullptr;
+	char* subStr = nullptr;
+	char** arr = nullptr;
+
+	init_string(str);
+	init_subString(subStr);
+	enter_letter(letter);
+
+	k = findNumberOfWords(str, cnt);
+
+	init_dvymerniy_array(arr, k);
+	arrayFill(str, arr);
+	addSubStr(arr, letter, subStr, k);
+
+	free_memory(str);
+	free_memory(subStr);
+	free_memory_dvymerniy_array(arr, k);
 
 	system("pause");
 	return 0;
