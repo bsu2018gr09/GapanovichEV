562e134ffc28fc230af2c92fca74fcb2287f9244
EugeneGapanovich <47712560+EugeneGapanovich@users.noreply.github.com>
2019-03-27 18:18:16 +0300

    Update 1-2.cpp
    Updated file

--- 1-2.cpp
+++ 1-2.cpp
@@ -1,4 +1,4 @@
-﻿//	Положительные элементы массива А(N) переставить в конец массива, сохраняя порядок следования. 
+//	Положительные элементы массива А(N) переставить в конец массива, сохраняя порядок следования. 
 //	Отрицательные элементы расположить в порядке убывания. Дополнительный массив не использовать.
 
 #include<iostream>
@@ -12,7 +12,7 @@ int* createMemory(int n) {
 	if (!Arr) {
 		cout << "Error!" << "\n";
 		system("pause");
-		return 0;
+		exit(0);
 	}
 	return Arr;
 }
@@ -21,17 +21,28 @@ void freeMemory(int*& Arr) {
 	Arr = nullptr;
 	return;
 }
-void initArrRandom(int* Arr, int n) {
-	for (int i = 0; i < n; ++i) {
-		*(Arr + i) = rand() % 40 - rand() % 30;
+void initArrRandom(int* Arr, int n){
+	int a, b;
+	cout << "Введите два положительных числа для заполнения массива случайными числами:\n";
+	cout << "1-е число: "; 
+	cin >> a;
+	cout << "2-е число: ";
+	cin >> b;
+	if (a <=0 || b <= 0) {
+		cout << "Нужно ввести положительные числа!\n";
+		system("pause");
+		exit(0);
 	}
+	else
+		for (int i = 0; i < n; ++i) {
+			*(Arr + i) = rand() % a - rand() % b;
+		}
 	return;
 }
 void printArr(int* Arr, int n) {
 	for (int i = 0; i < n; ++i) {
-		cout <<setw(4)<< *(Arr + i);
+		cout << setw(4) << *(Arr + i);
 	}
-	cout << "\n";
 	return;
 }
 void sort(int* Arr, int n) {
@@ -63,13 +74,15 @@ int main() {
 		system("pause");
 		exit(0);
 	}
-	int*Arr=createMemory(n);
+	int*Arr = createMemory(n);
 	initArrRandom(Arr, n);
 	cout << "Исходный массив: " << "\n";
 	printArr(Arr, n);
 	sort(Arr, n);
+	cout << "\n";
 	cout << "Сортированный массив: " << "\n";
 	printArr(Arr, n);
+	cout << "\n";
 	freeMemory(Arr);
 	system("pause");
 	return 0;
