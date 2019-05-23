496504bf06da0b865c99c4bdbd6d9c907239d8ba
EugeneGapanovich <47712560+EugeneGapanovich@users.noreply.github.com>
2019-05-23 08:45:48 +0300

    исправил

--- 5-1.cpp
+++ 5-1.cpp
@@ -29,15 +29,15 @@ int main() {
 	unsigned char r, g, b;
 	color c;
 	int step; // количество шагов
-	int cnt = 0; // счетчик
 	float r1, g1, b1; // первоначальные цвета (цвет 1)
 	float rr, gg, bb; // конечные цвета (цвет 2 к которому движемся)
 	float step_r, step_g, step_b;
+	int newstep;
 	float tmp_r, tmp_g, tmp_b;
 	unsigned int w, h;
 	/*	int step=100; // количество шагов
-		float r1=1, g1=100, b1=255; // первоначальные цвета (цвет 1)
-		float rr=100, gg=255, bb=9; // конечные цвета (цвет 2 к которому движемся) не работает при w=1024 */
+		float r1=0, g1=0, b1=0; // первоначальные цвета (цвет 1)
+		float rr=255, gg=255, bb=255; // конечные цвета (цвет 2 к которому движемся) не работает при w=1024 */
 	cout << "Введите пожалуйста шаг:";
 	cin >> step;
 
@@ -51,37 +51,44 @@ int main() {
 	ggg.write((char *)&buf, 18);
 	fff.read((char *)&w, 4); cout << "w=" << w;
 	fff.read((char *)&h, 4); cout << ", h=" << h;
-	w = 600;
-	h = 230;
+	w = 400;
+	h = 130;
 	ggg.write((char *)&w, 4);//не понял???? //- ширина файла rez.bmp изменяется на ширину файла 1.bmp//СПАСИБО КЭП. я не понял ЗАЧЕМ это делать????? Как мне по твоему поментять размер того, что я хочу????
 	ggg.write((char *)&h, 4);//не понял???? //- высота файла rez.bmp изменяется на высоту файла 1.bmp
 	fff.read((char *)&buf, 28);
 	ggg.write((char *)&buf, 28);
 
-	step_r = (rr - r1) / step;
-	step_g = (gg - g1) / step;
-	step_b = (bb - b1) / step;
+	if (step >= w)
+		step = w;
+
+	newstep = w / (step + 1);
+
+	if (!newstep)
+		newstep = 1;
+
+	step_r = (rr - r1) / ((w / newstep) - 1);
+	step_g = (gg - g1) / ((w / newstep) - 1);
+	step_b = (bb - b1) / ((w / newstep) - 1);
+
 
 	tmp_r = r1; tmp_g = g1; tmp_b = b1;
 
-	for (int i = 1; i <= h; ++i) {
-		for (int j = 1; j <= w; ++j) {
-			if (cnt < step) {
-				r1 += step_r;
-				g1 += step_g;
-				b1 += step_b;
-				++cnt;
+		for (int i = 1; i <= h; ++i) {
+			for (int j = 1; j <= w; ++j) {
+					if (((w - j) >= newstep) && ((j % newstep)==0)) {
+						r1 += step_r;
+						g1 += step_g;
+						b1 += step_b;
+					}
+				c.r = (unsigned char)r1;
+				c.g = (unsigned char)g1;
+				c.b = (unsigned char)b1;
+				ggg.write((char *)&c, 3);
 			}
-			c.r = (unsigned char)r1;
-			c.g = (unsigned char)g1;
-			c.b = (unsigned char)b1;
-			ggg.write((char *)&c, 3);
+			r1 = tmp_r;
+			g1 = tmp_g;
+			b1 = tmp_b;
 		}
-		r1 = tmp_r;
-		g1 = tmp_g;
-		b1 = tmp_b;
-		cnt = 0;
-	}
 	fff.close();
 	ggg.close();
 	//system("pause"); 
